04681748b7384ebb4f8865a0a5f1d8ab87bc9347
Tony Mputhia <tonymmputhia@gmail.com>
2023-09-08 12:32:21 +0300

    Get user input

--- main.c
+++ main.c
@@ -8,10 +8,20 @@
 int main(int argc, char **argv)
 {
 	char *display_prompt = "($)";
+	char *user_input = NULL;
+	size_t n = 0;
 
 	(void)argc;
-        (void)argv;
+	(void)argv;
 
 	printf("%s", display_prompt);
+	if ((getline(&user_input, &n, stdin)) == -1)
+	{
+		perror("Incorrect user input");
+		free(user_input);
+		exit(99);
+	}
+	printf("%s\n", user_input);
+	free(user_input);
 	return (0);
 }
