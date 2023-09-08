9de4d4562f4e4a4d2fa3a62eb2674d6637ee77ac
millyanne93 <nmillyanne20@gmail.com>
2023-09-08 02:12:39 -0700

    Display prompt

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed.
+ * @argv: The list of arguments.
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	char *display_prompt = "($)";
+
+	(void)argc;
+        (void)argv;
+
+	printf("%s", display_prompt);
+	return (0);
+}
