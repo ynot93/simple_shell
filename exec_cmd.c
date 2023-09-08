4c0f84e358409cdf228662ea918667e30a41db4f
millyanne93 <nmillyanne20@gmail.com>
2023-09-08 15:04:55 -0700

    Add enviroment variable to prototype

--- exec_cmd.c
+++ exec_cmd.c
@@ -1,18 +1,21 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
  * exec_cmd - executes commands from the input
  * @argv: List of arguments passed
+ * @envp: Environment variables
  *
  * Return: Nothing
  */
-void exec_cmd(char **argv)
+void exec_cmd(char **argv, char **envp)
 {
 	char *cmd = NULL;
 
 	if (argv)
 	{
 		cmd = argv[0];
-		if (execve(cmd, argv, NULL) == -1)
+		if (execve(cmd, argv, envp) == -1)
 		{
 			perror("Cannot execute command");
 		}
